7d8da9e612d739ee8a2cf6d4da3ca45ffeae100c
Dave Pagurek <dave@davepagurek.com>
2015-10-04 18:45:03 -0400

    Run it again

--- ynm_cli.rb
+++ ynm_cli.rb
@@ -2,11 +2,16 @@
 
 require_relative "./ynm.rb"
 interpreter = YNM::Interpreter.new("", Proc.new{|res| puts res.to_s})
+last = ""
 loop do
   printf("> ")
   line = gets
+  if line.gsub(/\s+/, "").length == 0
+    line = last
+  end
   break if /^(exit|quit)/i.match(line)
   interpreter.add_input!(line)
   interpreter.run!
+  last = line
 end
 
