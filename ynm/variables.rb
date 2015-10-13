66533f17c6a4eea3a33f5abdd07f5b1f5f577d37
Dave Pagurek <dave@davepagurek.com>
2015-10-13 10:42:14 -0400

    Faster autocorrection

--- ynm/variables.rb
+++ ynm/variables.rb
@@ -1,4 +1,6 @@
-module YNM
+module YNM 
+  require 'set'
+
   class Value
     def initialize(expression)
       @expression = expression
@@ -23,7 +25,7 @@ module YNM
     end
 
     def to_s
-      "some shit that can be done maybe"
+      "#{@expressions.length} shitty instruction#{@expressions.length==0 ? "" : "s"}"
     end
   end
 
@@ -48,7 +50,7 @@ module YNM
 
   class YNMString < Value
     require 'levenshtein'
-    DICTIONARY = File.read('dictionary.txt').split(/\s+/).group_by{|w| w[0]}
+    DICTIONARY = Set.new(File.read('dictionary.txt').split(/\s+/))
     LETTERS = "abcdefghijklmnopqrstuvwxyz".split("")
 
     def to_s
@@ -56,24 +58,49 @@ module YNM
     end
 
     def value
-      @expression.to_s[1..-2].split("").map do |char|
-        if /[a-z]/i.match(char) && rand > 0.8 
-          letter = LETTERS.sample 
-          char == char.upcase ? letter.upcase : letter
-        else
-          char
+      @expression.to_s[1..-2].scan(/(?:[a-z])+|[^a-z]+/i).map do |word|
+        next word if /[^a-z]/i.match(word) || rand > 0.8
+        copy_caps(mutations(word.downcase, 1).to_a.sample, word)
+      end.join("")
+    end
+
+    private
+    def deletes(word)
+      (0..word.length-1).map{|i| word[0..i-1]+word[i+1..-1]}
+    end
+    def transposes(word)
+      (1..word.length-1).map{|i| (word[0..i-1]+word[i+1..-1]).insert(i-1, word[i])}
+    end
+    def replaces(word)
+      (0..word.length-1).flat_map do |i|
+        LETTERS.map{|x| (word[0..i-1]+word[i+1..-1]).insert(i, x)}
+      end
+    end
+    def inserts(word)
+      (0..word.length-1).flat_map do |i|
+        LETTERS.map{|x| word.dup.insert(i, x)}
+      end
+    end
+    def mutations(word, levels, level=1)
+      variations = Set.new(deletes(word) + transposes(word) + replaces(word) + inserts(word))
+      if level < levels
+        variations.map!{|variation| mutations(variation, levels, level+1).add(variation)}
+      end
+      variations.flatten.keep_if{|variation| DICTIONARY.include?(variation)}.add(word)
+    end
+
+    def copy_caps(word, source)
+      if source == source.upcase
+        word.upcase
+      else
+        word.split("").each_with_index.map do |char, i|
+          if i < source.length && source[i] == source[i].upcase
+            char.upcase
+          else
+            char.downcase
+          end
         end
-      end.join("").split(/\s+/).map do |word|
-        if /^[a-z]/i.match(word)
-          DICTIONARY[word[0].downcase].map do |entry|
-           [entry, Levenshtein.distance(entry, word)]
-          end.sort do |a, b|
-            a[1] <=> b[1]
-          end.first[0]
-        else
-          word
-        end
-      end.join(" ")
+      end.join("")
     end
   end
 end
